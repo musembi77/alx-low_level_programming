081c9ae1afcbe861819a2ea9fde83d1eb96ce9ad
musembi77 <sammymusembi77@gmail.com>
2022-04-08 18:17:29 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
+/**
+ * main-Entry point
+ *
+ * Return: Always 0 (Success)
+ * */
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59;
+			return (1);
 }
